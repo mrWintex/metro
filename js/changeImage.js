73b668df4e8cd1c11f2c298b2b7817e970a7ee7b
Vít Prokeš <vprokes@u-sluno.cz>
2022-05-18 16:01:55 +0200

    responzivní design

--- js/changeImage.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $(".photo-option").on("click", function() {
-        $(".photo-wrapper").removeClass("d-flex").addClass("d-none");
-        $("#product-photo-"+$(this).attr("data-photo-id")).removeClass("d-none").addClass("d-flex");
-    });
-});
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function () {
     $(".photo-option").on("click", function () {
-        $(".photo-wrapper").removeClass("d-flex").addClass("d-none");
-        $("#product-photo-" + $(this).attr("data-photo-id")).removeClass("d-none").addClass("d-flex");
+        $(".slick-item-wrapper").removeClass("slick-active").removeClass("slick-current");
+        $("#product-photo-" + $(this).attr("data-photo-id")).addClass("slick-active").addClass("slick-current");
     });
 
     shown = false;
@@ -11,10 +11,29 @@ $(function () {
         $("#menu-list").css("display", (shown ? "block" : "none"));
     });
     $(document).on("click", function (e) {
-        if ($(e.target).closest("#menu-list").length === 0 && $(e.target).closest("#menu-list-button").length === 0) {
+        if ($(e.target).closest("#menu-list").length === 0 && $(e.target).closest("#menu-list-button").length === 0 && $(e.target).closest("#metro-menu-button").length === 0) {
             shown = false;
             $("#menu-list").css("display", "none");
             $("#menu-list-button").removeClass("active");
+            $(".navigation-bottom-mobile").removeClass("d-none").addClass("d-block");
+            $(".menu-list-header").addClass("d-none").removeClass("d-flex");
         }
+    });
+    $("#metro-menu-button").on("click", function(){
+        shown = !shown;
+        $("#menu-list").css("display", (shown ? "block" : "none"));
+        if(shown){
+            $(".navigation-bottom-mobile").removeClass("d-block").addClass("d-none"); 
+            $(".menu-list-header").removeClass("d-none").addClass("d-flex");
+        }
+        else{
+            $(".menu-list-header").addClass("d-none").removeClass("d-flex");
+            $(".navigation-bottom-mobile").removeClass("d-none").addClass("d-block"); 
+        }
+    });
+    $(".menu-item-link.root").on("click", function(){
+        $hasClass = $(this).hasClass("active");
+        $(".menu-item-link.root").removeClass("active");
+        $(this).toggleClass("active", !$hasClass);
     });
 });
